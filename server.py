f33dc3572a1f1f8638c5e6aebc94b69ba5544b76
shelleyyyyy <shelleywr23@mail.vmi.edu>
2022-04-18 19:46:37 -0400

    xsdv

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ class Server:
     def start_server(self):
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         
-        host = '192.168.1.206'
+        host = '10.13.18.76'
         port = int(input('Enter port to run the server on --> '))
 
         self.clients = []
